b36168908b207eab28d0bb1050ecf1bcb8a75611
Sohini <sohinichat08@gmail.com>
2025-08-01 12:15:45 +0800

    Update streamlitest.py

--- streamlitest.py
+++ streamlitest.py
@@ -1,3 +1,11 @@
 import random 
 a = random.randint(1,10)
 print(a)
+
+
+import streamlit as st
+st.title("Test App")
+st.header("numbers")
+st.subheader("ai club)
+st.write("I love AIclub")
+            
