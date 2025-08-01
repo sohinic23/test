0f70a9984f4988be538de701138c2a246c962cd3
Sohini <sohinichat08@gmail.com>
2025-08-01 12:21:16 +0800

    Update streamlitest.py

--- streamlitest.py
+++ streamlitest.py
@@ -3,9 +3,14 @@ import streamlit as st
 import random 
 a = random.randint(1,10)
 st.write(a)
+num = st.text_input("Hello, Please enter a number and I will print all  umbers less than it..")
+num = int(num)
 
-for i in range(0,10):
+
+for i in range(0,num):
             st.write(i)
+
+
 
 
 st.title("Test App")
